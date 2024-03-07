c2a36b2829e6d6dda904a605f0ae37a58fb60ddb
David Sawatzke <david@swabianinstruments.com>
2024-03-07 13:36:49 +0100

    Add script to format all files

---
+++ format.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+cd $(dirname "$0")
+
+find hdl/ tb/ target/*/hdl/ -iname "*.sv" -print0 | xargs -0 -n1 verible-verilog-format --flagfile .verible-verilog-format.conf --inplace
+find host/ target/*/host/ scripts/ test/*.py -iname "*.py" -print0 | xargs -0 autopep8 --exit-code -i
