9ec37b0c7fb1e965bf84b8772fe1dfb2bbf7c1ce
Ehsan Jokar <ehsan@swabianinstruments.com>
2024-03-14 14:14:21 +0000

    using forwarding technique

--- hdl/histogram/Hist_1lane.sv
+++ hdl/histogram/Hist_1lane.sv
@@ -7,7 +7,7 @@
  * Copyright (C) 2023 Swabian Instruments, All Rights Reserved
  *
  * Authors:
- * - 2023 Ehsan Jokar <ehsan@swabianinstruments.com>
+ * - 2023-2024 Ehsan Jokar <ehsan@swabianinstruments.com>
  *
  * This file is provided under the terms and conditions of the BSD 3-Clause
  * license, accessible under https://opensource.org/licenses/BSD-3-Clause.
@@ -21,6 +21,26 @@
  `default_nettype none
 // verilog_format: on
 
+//////////////////////////////////////////////////////////////////////////////////
+
+/* This module efficiently implements the accumulation operation "if(enable) array[index]++", offering rapid
+   random access index and a one-cycle initiation interval for optimal performance.
+
+   To optimize performance, it employs a pipelined infrastructure for fetching, forwarding, accumulating, and
+   writing back data. The pipeline comprises the following stages, each serving distinct purposes and synchronized
+    with input registers:
+
+ 1. BRAM read operation stage: Handles the retrieval of data from BRAM. (Inputs: valid_in, rd_addr)
+ 2. BRAM output registers and register forwarding analysis stage: Manages the output registers of BRAM and
+    performs register forwarding analysis. (Inputs: r1valid_in, r1rd_addr)
+ 3. Early-forwarding stage (4:1 MUX): Utilizes a 4:1 multiplexer for early data forwarding.
+    (Inputs: r2valid_in, r2rd_addr, BRAM_dout, forward_with_***_cycle_delay)
+ 4. Late-forwarding stage (2:1 MUX), shifter, and accumulator: Incorporates a 2:1 multiplexer, shifter,
+    and accumulator for late data forwarding and accumulation. (Inputs: r3valid_in, r3rd_addr, DO_forward)
+ 5. Writeback stage: Manages the writing back of accumulated data. (Inputs: wt_en, wt_addr, BRAM_din)
+ */
+
+//////////////////////////////////////////////////////////////////////////////////
 
 module Hist_1lane #(
     parameter HIST_MEM_DEPTH = 4096,
@@ -37,100 +57,101 @@ module Hist_1lane #(
     output reg last_sample
 );
 
+    logic [HIST_MEM_ADDR_WIDTH-1:0] rd_addr;
+    logic [     HIST_WORD_SIZE-1:0] DO_register;
+    logic [     HIST_WORD_SIZE-1:0] BRAM_dout;
+    logic [HIST_MEM_ADDR_WIDTH-1:0] wt_addr;
+    logic [     HIST_WORD_SIZE-1:0] BRAM_din;
+    logic                           wt_en;
 
-    // defining the memory ports
-    logic [HIST_MEM_ADDR_WIDTH - 1 : 0] addra, addrb;
-    logic [HIST_WORD_SIZE - 1 : 0] dina, doutb;
-    logic ena, wea, enb;
+    (* ram_style="block" *)reg   [ HIST_WORD_SIZE - 1 : 0] BRAM        [HIST_MEM_DEPTH] = '{default: '0};
+    always @(posedge clk) begin
+        if (wt_en) BRAM[wt_addr] <= BRAM_din;
 
-    // xpm_memory_sdpram: Simple Dual Port RAM
-    // Xilinx Parameterized Macro, version 2023.2
-
-    xpm_memory_sdpram #(
-        .ADDR_WIDTH_A(HIST_MEM_ADDR_WIDTH),
-        .ADDR_WIDTH_B(HIST_MEM_ADDR_WIDTH),
-        .BYTE_WRITE_WIDTH_A(HIST_WORD_SIZE),
-        .CLOCKING_MODE("common_clock"),
-        .MEMORY_OPTIMIZATION("true"),
-        .MEMORY_SIZE(HIST_MEM_DEPTH * HIST_WORD_SIZE),
-        .READ_DATA_WIDTH_B(HIST_WORD_SIZE),
-        .READ_LATENCY_B(1),
-        .WRITE_DATA_WIDTH_A(HIST_WORD_SIZE),
-        .WRITE_PROTECT(1)
-    ) xpm_memory_sdpram_inst (
-        .doutb(doutb),  // READ_DATA_WIDTH_B-bit output: Data output for port B read operations.
-        .addra(addra),  // ADDR_WIDTH_A-bit input: Address for port A write operations.
-        .addrb(addrb),  // ADDR_WIDTH_B-bit input: Address for port B read operations.
-        .clka (clk),    // 1-bit input: Clock signal for port A. Also clocks port B when
-                        // parameter CLOCKING_MODE is "common_clock".
-
-        .dina(dina),  // WRITE_DATA_WIDTH_A-bit input: Data input for port A write operations.
-        .ena (ena),   // 1-bit input: Memory enable signal for port A. Must be high on clock
-                      // cycles when write operations are initiated. Pipelined internally.
-
-        .enb(enb),  // 1-bit input: Memory enable signal for port B. Must be high on clock
-                    // cycles when read operations are initiated. Pipelined internally.
-
-        .regceb(1),  // 1-bit input: Clock Enable for the last register stage on the output
-                     // data path.
-        .wea(wea)
-    );
-
-    // End of xpm_memory_sdpram_inst instantiation
-
-    // registering the input address and the corresponding valid
-    logic [HIST_MEM_ADDR_WIDTH - 1 : 0] r1address_in;
-    logic r1valid_in;
-    always_ff @(posedge clk) begin
-        r1valid_in   <= valid_in;
-        r1address_in <= address_in;
+        DO_register <= BRAM[rd_addr];
+        BRAM_dout   <= DO_register;  // BRAM output register for better timing
     end
 
-    // reading data from the memory
-    // When valid_in is asserted, we read the data from the corresponding
-    // address if we have not received the same address in the last
-    // clock. It means that if we receive an address in which we've received
-    // it in the last clock cycle, we haven't yet write back the corresponding
-    // result into the memory. Therefore, we don't read again from the same
-    // address. In other words, we don't read from the same address until
-    // it's repeated in consecutive clock cycles.
-    assign enb   = (r1valid_in && (address_in == r1address_in)) ? 0 : valid_in;
-    // assign addrb  = address_in;
-    assign addrb = enb ? address_in : 'X;
+    logic [HIST_WORD_SIZE-1:0] r1BRAM_din, r2BRAM_din;
+    logic r1valid_in, r2valid_in, r3valid_in;
+    logic [HIST_MEM_ADDR_WIDTH-1:0] r1rd_addr, r2rd_addr, r3rd_addr;
 
-    // writing back data into the memory
-    // When we read a data from an address, the incremented data is expected to
-    // be written back into the memory in the next two clock cycles. However,
-    // a data is not written into an address if we receive the same address in
-    // the next clock cycle. It means that we don't write a data
-    // into an address while we receive the same address in consecutive clock cycles.
-    // In addition, when the memory is read out, and the hist_rst is not asserted,
-    // no data should be written into the memory.
-    assign ena   = ((valid_in && (r1address_in == address_in)) || (hist_read && !hist_rst)) ? 0 : r1valid_in;
-    assign wea   = ena;
+    assign rd_addr = address_in;
+    always @(posedge clk) begin
+        r1BRAM_din <= BRAM_din;
+        r2BRAM_din <= r1BRAM_din;
 
-    // writing data into the memory
-    logic [HIST_WORD_SIZE - 1:0] counter, counter_reg;
-    logic r1enb;
-    // registering the value of counter and enb
-    always_ff @(posedge clk) begin
-        counter_reg <= counter;
-        r1enb <= enb;
-    end
-    // when r1enb is one, the new data has been read from the memory; so, the counter
-    // should be initialized with this data added by one. Otherwise, the counter should
-    // be incremented.
-    assign counter = r1enb ? doutb + 1 : r1valid_in ? counter_reg + 1 : 'X;
-    // the counter should be write into the BRAM
-    assign dina = hist_rst ? 0 : ena ? counter : 'X;
-    assign addra = ena ? r1address_in : 'X;
+        r1valid_in <= valid_in;
+        r2valid_in <= r1valid_in;
+        r3valid_in <= r2valid_in;
 
-    // generarting the output
-    always_ff @(posedge clk) begin
-        valid_out <= hist_read ? r1enb : 0;
-        data_out <= (hist_read && r1enb) ? doutb : 'X;
-        last_sample <= 0;
-        if (hist_read && r1enb && r1address_in == HIST_MEM_DEPTH - 1) last_sample <= 1;
+        r1rd_addr <= rd_addr;
+        r2rd_addr <= r1rd_addr;
+        r3rd_addr <= r2rd_addr;
+
+        wt_addr <= r3rd_addr;
+        wt_en <= r3valid_in;
     end
 
+    logic [HIST_WORD_SIZE-1:0] DO_forward;
+    logic forward_with_one_cycle_delay;
+    logic forward_with_two_cycle_delay;
+    logic forward_with_three_cycle_delay;
+    logic forward_with_four_cycle_delay_pre;
+    logic forward_with_four_cycle_delay;
+    logic [1:0] forward_mux_index;
+
+    always @(posedge clk) begin
+        /*If the location to be read was written within the last four clock cycles, refrain from reading from
+            RAM and instead utilize the forwarded value. The following conditions always  wt_en && (wt_addr == rd_addr),
+            with varying latencies as indicated by the forward_with variables' names.
+           */
+        forward_with_one_cycle_delay <= r2valid_in && (r2rd_addr == r1rd_addr);
+        forward_with_two_cycle_delay <= r3valid_in && (r3rd_addr == r1rd_addr);
+        forward_with_three_cycle_delay <= wt_en && (wt_addr == r1rd_addr);
+        forward_with_four_cycle_delay_pre <= wt_en && (wt_addr == rd_addr);
+        forward_with_four_cycle_delay <= forward_with_four_cycle_delay_pre;
+
+        /* For forwards with more than one cycle delay, we can select the corresponding value one cycle earlier, thereby
+            enhancing timing. Additionally, provide Vivado with the hint to implement this as a simple 4:1 MUX. This not only
+            reduces resource utilization but also enhances timing after the BRAM data output ports.
+            */
+        forward_mux_index = forward_with_one_cycle_delay       ? 2'hX :
+                                forward_with_two_cycle_delay   ? 2'h0 :
+                                forward_with_three_cycle_delay ? 2'h1 :
+                                forward_with_four_cycle_delay  ? 2'h2 :
+                                                                 2'h3;
+
+        DO_forward <= forward_mux_index == 2'h0 ?     BRAM_din   :
+                          forward_mux_index == 2'h1 ? r1BRAM_din :
+                          forward_mux_index == 2'h2 ? r2BRAM_din :
+                          forward_mux_index == 2'h3 ? BRAM_dout  :
+                          {HIST_WORD_SIZE{1'bX}};
+    end
+
+    logic [HIST_WORD_SIZE-1:0] var_DO_with_forwards;
+    logic forward_with_one_cycle_delay_p;
+
+    /* Late forwarding: The read address might remain constant for two clock cycles. Therefore, the selection between
+    the value of the last clock cycle or the value of the forward engine must be implemented in the same stage as
+    the accumulator to achieve the required one-cycle latency.
+    */
+    assign var_DO_with_forwards = forward_with_one_cycle_delay_p ? BRAM_din : DO_forward;
+
+    logic r1hist_rst, r2hist_rst, r3hist_rst;
+    always @(posedge clk) begin
+        forward_with_one_cycle_delay_p <= forward_with_one_cycle_delay;
+
+        r1hist_rst <= hist_rst;
+        r2hist_rst <= r1hist_rst;
+        r3hist_rst <= r2hist_rst;
+
+        BRAM_din <= r3hist_rst ? 0 : var_DO_with_forwards + 1;
+
+        valid_out <= hist_read ? r2valid_in : 0;
+        data_out <= hist_read ? BRAM_dout : 'X;
+
+        // This signal indicates the completion of reading out and resetting the block RAM
+        last_sample <= hist_read && wt_en && wt_addr == HIST_MEM_DEPTH - 1;
+    end
 endmodule
